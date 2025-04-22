cbf6bbc353db1c7529ef4b32182cbc90e6ed5c68
Rylie Castell <rylie.castell@gmail.com>
2025-04-22 12:09:27 +0200

    add preparation

---
+++ RPG/Pseudocode.swift
@@ -0,0 +1,31 @@
+//
+//  Pseudocode.swift
+//  RPG
+//
+//  Created by Rylie Castell on 22.04.25.
+//
+
+import Foundation
+
+// - add first character to party
+// - start first fight
+//      - print round x starts
+//      - print description for characters and opponents with status values
+//      - check status for each character
+//      - if character is able to fight:
+//          - choose between attack and use item
+//          - modify status values accordingly
+//      - choose move of opponent
+//          - add henchman / modify status values
+//      - check status for each character and opponent
+//      - repeat if both sides are undefeated
+//      - end fight and print result
+//      - add XP and found items
+//  - randomly decide to meet new character
+//  - decide to start next fight or go to tavern
+//  - go to tavern
+//      - heal all party members
+//      - exchange party members if wanted
+//  - start next fight
+//  - repeat until given amount of fights are 0
+//  - end game
