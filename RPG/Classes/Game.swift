552d0b04e66fe8c9c3fa388d4f727bbe514f9c3e
Rylie Castell <rylie.castell@gmail.com>
2025-04-25 00:32:16 +0200

    add game class and unfinished functions

--- RPG/Classes/Game.swift
+++ RPG/Classes/Game.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+class Game {
+    
+    var party: [Hero] = []
+    
+    var hero1: Hero = Assassin()
+    var hero2: Hero = Ranger()
+    var hero3: Hero = Sorcerer()
+    var hero4: Hero = Thief()
+    
+    var bag: Bag = Bag(items: [])
+    
+    var currentOpponents: [Opponent] = []
+    
+    var fightIsRunning: Bool = false
+    
+    func checkIfBothSidesCanFight() -> Bool {
+        if party.contains(where: { $0.isAlive }) && currentOpponents.contains(where: { $0.isAlive }) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func printAllStatusInfos() {
+        party.forEach {
+            print($0)
+        }
+        currentOpponents.forEach {
+            print($0)
+        }
+    }
+    
+    func fight(opponent: Opponent) {
+        print("\(opponent.name) appears. The fight begins!")
+        fightIsRunning = true
+        var roundCounter: Int = 0
+        while fightIsRunning {
+            if checkIfBothSidesCanFight() {
+                roundCounter += 1
+                print("Round \(roundCounter)".highlight())
+                printAllStatusInfos()
+                party.shuffle()
+            } else {
+                fightIsRunning = false
+            }
+        }
+        party.contains(where: { $0.isAlive }) ? print("You won!") : print("You lost!")
+    }
+    
+    func run() {}
+    
+    init() {}
+}
