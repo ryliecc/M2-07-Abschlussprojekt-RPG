c52d59ea4555d4c5cf7d76e1ede1ca5cd8c2e508
Rylie Castell <rylie.castell@gmail.com>
2025-05-11 13:56:36 +0200

    add status bar class

--- RPG/Classes/StatusBar.swift
+++ RPG/Classes/StatusBar.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+class StatusBar: CustomStringConvertible {
+    
+    var dayTime: TimeOfDay
+    var coins: Int
+    var party: [Hero]
+    
+    var healthStatuses: [String:HealthStatus] {
+        var dictionary: [String:HealthStatus] = [:]
+        for hero in party {
+            if hero.isAlive {
+                let percentage = hero.maxHealthPoints / hero.healthPoints
+                switch percentage {
+                case ..<0.3:
+                    dictionary[hero.name] = .critical
+                case ..<0.6:
+                    dictionary[hero.name] = .injured
+                default:
+                    dictionary[hero.name] = .healthy
+                }
+            } else {
+                dictionary[hero.name] = .dead
+            }
+        }
+        return dictionary
+    }
+    
+    var description: String {
+        ""
+    }
+    
+    init(dayTime: TimeOfDay, coins: Int, party: [Hero]) {
+        self.dayTime = dayTime
+        self.coins = coins
+        self.party = party
+    }
+}
